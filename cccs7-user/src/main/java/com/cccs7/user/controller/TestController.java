00274408a5b2ce40aa049c4f86f3d27daa907c31
cs7eric <Cs7eric@outlook.com>
2023-07-29 17:07:35 +0800

    feat:2023/07/29集成log4j、异步日志

--- cccs7-user/src/main/java/com/cccs7/user/controller/TestController.java
+++ cccs7-user/src/main/java/com/cccs7/user/controller/TestController.java
@@ -1,12 +1,14 @@
 package com.cccs7.user.controller;
 
 import com.cccs7.redis.util.RedisShareLockUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/test")
 public class TestController {
@@ -31,5 +33,16 @@ public class TestController {
     public void testLock() {
         boolean res = redisShareLockUtil.lock("cccs7", "1111111", 100000L);
         System.out.println(res);
+    }
+
+    @GetMapping("/testLog")
+    public void testLog() {
+
+        long startTime = System.currentTimeMillis();
+        for (int i = 0; i < 100000; i++) {
+            log.info("这是{}条日志！", i);
+        }
+        long endTime = System.currentTimeMillis();
+        log.info("当前耗时：{}", endTime - startTime);
     }
 }
