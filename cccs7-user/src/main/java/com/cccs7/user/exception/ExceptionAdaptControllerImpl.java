ad8a0ad27ab09090109dbdbc6da91a4445dbee8f
cs7eric <Cs7eric@outlook.com>
2023-08-08 13:34:27 +0800

    feat:2023/08/08 修复全局异常与security 冲突

---
+++ cccs7-user/src/main/java/com/cccs7/user/exception/ExceptionAdaptControllerImpl.java
@@ -0,0 +1,8 @@
+package com.cccs7.user.exception;/**
+ *  <p> 异常处理器 </p>
+ *
+ * @Author cccs7/cs7eric - csq020611@gmail.com
+ * @Description 异常处理器
+ * @Date 2023/8/8 13:00
+ */public class ExceptionAdaptControllerImpl {
+}
