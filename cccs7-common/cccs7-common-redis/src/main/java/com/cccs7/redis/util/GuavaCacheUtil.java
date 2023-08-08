28ee6593f91287ec2b0e6bdb98d8a3eef7948082
cs7eric <Cs7eric@outlook.com>
2023-08-08 14:56:46 +0800

    feat:2023/08/08guava本地缓存

---
+++ cccs7-common/cccs7-common-redis/src/main/java/com/cccs7/redis/util/GuavaCacheUtil.java
@@ -0,0 +1,16 @@
+package com.cccs7.redis.util;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * <p> 本地缓存工具类 </p>
+ *
+ * @Author cccs7/cs7eric - csq020611@gmail.com
+ * @Description 本地缓存工具类
+ * @Date 2023/8/8 14:09
+ */
+@Component
+public class CacheUtil {
+
+
+}
